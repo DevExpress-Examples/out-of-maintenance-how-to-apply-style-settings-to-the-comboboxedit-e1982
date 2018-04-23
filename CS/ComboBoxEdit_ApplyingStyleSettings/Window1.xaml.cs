8f012d286e530377d2da3219d0361f45c170731c
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/ComboBoxEdit_ApplyingStyleSettings/Window1.xaml.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows;
+
+namespace ComboBoxEdit_ApplyingStyleSettings {
+    public partial class Window1 : Window {
+        public Window1() {
+            InitializeComponent();
+        }
+    }
+}

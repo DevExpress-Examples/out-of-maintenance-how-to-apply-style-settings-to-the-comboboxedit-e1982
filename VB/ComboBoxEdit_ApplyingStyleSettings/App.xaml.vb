74c0442200464e4f256c771dd9eda4dfd5a3193c
DevExpressExampleBot <fakeEmail>
2022-12-16 18:46:16 +0400

    Source auto update [skip ci]

---
+++ VB/ComboBoxEdit_ApplyingStyleSettings/App.xaml.vb
@@ -0,0 +1,12 @@
+Imports System.Windows
+
+Namespace ComboBoxEdit_ApplyingStyleSettings
+
+    ''' <summary>
+    ''' Interaction logic for App.xaml
+    ''' </summary>
+    Public Partial Class App
+        Inherits Application
+
+    End Class
+End Namespace
